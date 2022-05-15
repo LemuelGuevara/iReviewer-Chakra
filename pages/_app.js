2c7a3522e8904930e339d7e58fea4d7903141938
RiderxL <lemuelkuya@gmail.com>
2022-05-15 12:14:27 +0800

    added a bit of animations

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,35 @@
 import "../styles/globals.css";
-// // import "focus-visible/dist/focus-visible";
-// import { AppProps } from "next/app";
-// import { ChakraProvider } from "@chakra-ui/react";
-// import theme from "../theme/theme";
-// import { SessionProvider } from "next-auth/react";
-
-// function App({ Component, pageProps: { session, ...pageProps} }: AppProps) {
-//   return (
-//     <SessionProvider session={session}>
-//       <ChakraProvider theme={theme}>
-//         <Component {...pageProps} />
-//       </ChakraProvider>
-//     </SessionProvider>
-//   );
-// }
-
-// export default App;
-
 import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
-// import { RecoilRoot } from "recoil";
-// import { ChakraProvider } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import theme from "../theme/theme";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
+  router,
 }) {
   return (
     // `session` comes from `getServerSideProps` or `getInitialProps`.
     // Avoids flickering/session loading on first load.
     <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+      <motion.div
+        key={router.router}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
         </ChakraProvider>
+      </motion.div>
     </SessionProvider>
   );
 }
