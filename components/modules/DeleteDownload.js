ef6eeeb625adaf20cb675a107735c321b4935e51
RiderxL <lemuelkuya@gmail.com>
2022-05-18 07:51:35 +0800

    Made the download function download without redirects

--- components/modules/DeleteDownload.js
+++ components/modules/DeleteDownload.js
@@ -4,29 +4,25 @@ import { useRouter } from "next/router";
 import { useSession, getProviders, getSession } from "next-auth/react";
 import { onSnapshot } from "firebase/firestore";
 import { db } from "../../app/firebaseApp";
-import {
-  MdOutlineCloudDownload,
-  MdDelete,
-} from "react-icons/md";
-import {
-  Flex,
-  Stack,
-  Button,
-  Icon,
-} from "@chakra-ui/react";
-import { addDoc, collection, doc, deleteDoc } from "@firebase/firestore";
+import { MdOutlineCloudDownload, MdDelete } from "react-icons/md";
+import { Flex, Stack, Button, Icon } from "@chakra-ui/react";
+import { doc, deleteDoc } from "@firebase/firestore";
 import { deleteObject, ref, getStorage } from "firebase/storage";
-import { pdfRef } from "../modules/UploadModal"
 
 function DeleteDownload({ title }) {
   const { data: session } = useSession();
   const router = useRouter();
   const { id } = router.query;
+
+  // States
   const [reviewers, setReviewers] = useState([]);
+  const [fetching, setFetching] = useState(false);
+  const [error, setError] = useState(false);
+
 
   // Get firebase storage
-  const storage = getStorage()
-  const pdfRef = ref(storage, `reviewers/${id}/${title}.pdf`)
+  const storage = getStorage();
+  const pdfRef = ref(storage, `reviewers/${id}/${title}.pdf`);
 
   useEffect(
     () =>
@@ -36,12 +32,33 @@ function DeleteDownload({ title }) {
     [id]
   );
 
-  // Delete firebase file
+  // Download firebase file
+  const download = (url, name) => {
+    if (!url) {
+      throw new Error("Resource URL not provided! You need to provide one");
+    }
+    setFetching(true);
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        setFetching(false);
+        const blobURL = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = blobURL;
+        a.style = "display: none";
 
+        if (name && name.length) a.download = name;
+        document.body.appendChild(a);
+        a.click();
+      })
+      .catch(() => setError(true));
+  };
+
+  // Delete firebase file
   const deleteFile = async () => {
-    deleteDoc(doc(db, "reviewers", id))
-    deleteObject(pdfRef)
-  }
+    deleteDoc(doc(db, "reviewers", id));
+    deleteObject(pdfRef);
+  };
 
   return (
     <div>
@@ -56,32 +73,26 @@ function DeleteDownload({ title }) {
                 fontWeight={600}
                 onClick={(e) => {
                   e.stopPropagation();
-                  deleteFile()
+                  deleteFile();
                   router.push("/");
                 }}
               >
                 <Icon as={MdDelete} fontSize="lg" />
               </Button>
-              <a
-                target="_blank"
-                href={reviewers?.pdf}
-                rel="noopener noreferrer"
-              >
                 <Button
                   variant={"primary-md"}
                   size={"sm"}
                   px={3}
                   fontWeight={600}
                   leftIcon={<MdOutlineCloudDownload />}
+                  onClick={()=> download(reviewers?.pdf, title)}
                 >
                   Download
                 </Button>
-              </a>
             </Stack>
           </div>
         ) : (
           <div>
-            <a target="_blank" href={reviewers?.pdf} rel="noopener noreferrer">
               <Button
                 variant={"primary-md"}
                 size={"sm"}
@@ -89,10 +100,10 @@ function DeleteDownload({ title }) {
                 // display={{ base: "none", md: "flex" }}
                 fontWeight={600}
                 leftIcon={<MdOutlineCloudDownload />}
+                onClick={()=> download(reviewers?.pdf, title)}
               >
                 Download
               </Button>
-            </a>
           </div>
         )}
       </Flex>
